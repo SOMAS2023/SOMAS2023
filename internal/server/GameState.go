8b559813dbb2d258d05c45ec5060a1e221a2a055
sheriff4000 <sheriff4000@gmail.com>
2023-11-09 18:27:38 +0000

    add megabikes to gamestate

--- internal/server/GameState.go
+++ internal/server/GameState.go
@@ -12,6 +12,7 @@ The GameState is an implementation of the IGameState interface
 type GameState struct {
 	BaseBikers map[uuid.UUID]obj.IBaseBiker // Map of IDs to IBaseBiker objects
 	LootBoxes  map[uuid.UUID]obj.LootBox    // Map of IDs to LootBox objects
+	MegaBikes  map[uuid.UUID]obj.MegaBike    // Map of IDs to LootBox objects
 }
 
 func (g GameState) GetGameState() utils.IGameState {
