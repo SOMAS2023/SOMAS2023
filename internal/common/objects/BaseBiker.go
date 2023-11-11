8bd5a4a24a041095702c169354ac4f143def752a
lupisdupis <lucianecchi01@gmail.com>
2023-11-11 13:57:50 +0000

    added comments to base biker

--- internal/common/objects/BaseBiker.go
+++ internal/common/objects/BaseBiker.go
@@ -13,15 +13,14 @@ import (
 // agent with defualt strategy for MVP:
 type IBaseBiker interface {
 	baseAgent.IAgent[IBaseBiker]
-	// DecideAction determines what action the agent is going to take this round.
 	// Based on this, the server will call either DecideForce or ChangeBike
-	DecideAction() BikerAction
+	DecideAction() BikerAction                                             // determines what action the agent is going to take this round. Based on this, the server will call either DecideForce or ChangeBike
 	DecideForce() utils.Forces                                             // defines the vector you pass to the bike: [pedal, brake, turning]
-	ChangeBike() uuid.UUID                                                 // action never performed in MVP, might call PickPike() in future implementations
+	ChangeBike() uuid.UUID                                                 // called when biker wants to change bike, it will choose which bike to try and join based on agent-specific strategies
 	UpdateColour(totColours utils.Colour)                                  // called if a box of the desired colour has been looted
 	UpdateAgent(energyGained float64, energyLost float64, pointGained int) // called by server
-	GetLocation() utils.Coordinates
-	UpdateGameState(gameState IGameState)
+	GetLocation() utils.Coordinates                                        // gets the agent's location
+	UpdateGameState(gameState IGameState)                                  // sets the gameState field at the beginning of each round
 }
 
 type BikerAction int
@@ -40,28 +39,29 @@ const (
 // - when biker dies the server deletes the instance (can have it set a status field "alive" to false instead if we
 // want to keep a record of dead bikers)
 // - server keeps track of round number
-// - asssume server assigns initial bikes to ppl
-
-// What we need to know:
-// - How is loot location given to us? (ie utils.IGameState)
-// - How does the physics engine work
+// - assume server assigns initial bikes to ppl
 
 type BaseBiker struct {
 	*baseAgent.BaseAgent[IBaseBiker]              // BaseBiker inherits functions from BaseAgent such as GetID(), GetAllMessages() and UpdateAgentInternalState()
 	soughtColour                     utils.Colour // the colour of the lootbox that the agent is currently seeking
-	onBike                           bool
-	energyLevel                      float64 // float between 0 and 1
-	points                           int
-	alive                            bool
-	megaBikeId                       uuid.UUID
-	gameState                        IGameState
+	onBike                           bool         // whether an agent is on a bike or not. The game allows for agents to not be on a bike during a round, in that case they will technically be "outside" of the map in a sort of idle state
+	energyLevel                      float64      // float between 0 and 1
+	points                           int          // tracks the number of lootboxes of the right colour have been looted
+	alive                            bool         // an agent can die if: 1. they run out of energy (energyLevel = 0) 2. they get hit by the Audi
+	megaBikeId                       uuid.UUID    // if they are not on a bike it will be 0
+	gameState                        IGameState   // updated by the server at every round
 }
 
+// the biker itself doesn't technically have a location (as it's on the map only when it's on a bike)
+// in fact this function is only called when the biker needs to make a decision about the pedaling forces
 func (bb *BaseBiker) GetLocation() utils.Coordinates {
 	megaBikes := bb.gameState.GetMegaBikes()
 	return megaBikes[bb.megaBikeId].coordinates
 }
 
+// returns the nearest lootbox with respect to the agent's bike current position
+// in the MVP this is used to determine the pedalling forces as all agent will be
+// aiming to get to the closest lootbox by default
 func (bb *BaseBiker) NearestLoot() utils.Coordinates {
 	currLocation := bb.GetLocation()
 	shortestDist := math.MaxFloat64
@@ -78,16 +78,19 @@ func (bb *BaseBiker) NearestLoot() utils.Coordinates {
 	return nearestDest
 }
 
+// in the MVP the biker's action defaults to pedaling (as it won't be able to change bikes)
+// in future implementations this function will be overridden by the agent's specific strategy
+// which will be used to determine whether to pedalor try to change bike
 func (bb *BaseBiker) DecideAction() BikerAction {
 	return Pedal
 }
 
-// once we know what utils.IGameState looks like we can pass what we need (ie maybe just lootboxes and info on our bike)
-// currently taking gamestate as having everything
+// determine the forces (pedalling, breaking and turning)
+// in the MVP the pedalling force will be 1, the breaking 0 and the tunring is determined by the
+// location of the nearest lootbox
 func (bb *BaseBiker) DecideForce() utils.Forces {
-	// the way this is determined depends on how the physics engine works and on what exactly the server passes us
 
-	// NEAREST BOX STRATEGY
+	// NEAREST BOX STRATEGY (MVP)
 	currLocation := bb.GetLocation()
 	nearestLoot := bb.NearestLoot()
 	deltaX := nearestLoot.X - currLocation.X
@@ -105,14 +108,17 @@ func (bb *BaseBiker) DecideForce() utils.Forces {
 }
 
 // decide which bike to go to
+// for now it just returns a random uuid
 func (bb *BaseBiker) ChangeBike() uuid.UUID {
 	return uuid.New()
 }
 
+// this is called when a lootbox of the desidered colour has been looted in order to update the sought colour
 func (bb *BaseBiker) UpdateColour(totColours utils.Colour) {
 	bb.soughtColour = utils.Colour(rand.Intn(int(totColours)))
 }
 
+// update the energy levels and points at the end of a round
 func (bb *BaseBiker) UpdateAgent(energyGained float64, energyLost float64, pointsGained int) {
 	bb.energyLevel += (energyGained - energyLost)
 	bb.points += pointsGained
