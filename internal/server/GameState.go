69f3af21acd658af74455372c74e234c85d5a66f
Basheq Tarifi <btarifi08@gmail.com>
2023-11-12 17:40:06 +0000

    (fix) Fix imports in GameState and MegaBike

--- internal/server/GameState.go
+++ internal/server/GameState.go
@@ -2,7 +2,7 @@ package server
 
 import (
 	obj "SOMAS2023/internal/common/objects"
-	"SOMAS2023/internal/common/utils"
+
 	"github.com/google/uuid"
 )
 
@@ -12,15 +12,15 @@ The GameState is an implementation of the IGameState interface
 type GameState struct {
 	BaseBikers map[uuid.UUID]obj.IBaseBiker // Map of IDs to IBaseBiker objects
 	LootBoxes  map[uuid.UUID]obj.LootBox    // Map of IDs to LootBox objects
-	MegaBikes  map[uuid.UUID]obj.MegaBike    // Map of IDs to LootBox objects
+	MegaBikes  map[uuid.UUID]obj.MegaBike   // Map of IDs to LootBox objects
 }
 
-func (g GameState) GetGameState() utils.IGameState {
+func (g GameState) GetGameState() obj.IGameState {
 	//TODO implement me
 	panic("implement me")
 }
 
-// The usuage of GameState would be as follows:
+// The usage of GameState would be as follows:
 
 // // Add a Biker to the GameState
 // bikerID := "biker1"
