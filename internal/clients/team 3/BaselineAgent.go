df706887dffb7995e0aa9bd28e30271f13ac9eec
ih8gin <erictxl@outlook.com>
2023-11-18 13:31:05 +0000

    implement func in baseline

--- internal/clients/team 3/BaselineAgent.go
+++ internal/clients/team 3/BaselineAgent.go
@@ -2,12 +2,16 @@ package team_3
 
 import (
 	"SOMAS2023/internal/common/objects"
+	"SOMAS2023/internal/common/physics"
 	"SOMAS2023/internal/common/utils"
 	"github.com/google/uuid"
+	"math"
+	"sort"
 )
 
 type BaselineAgent struct {
 	objects.BaseBiker
+	currentBike *objects.MegaBike
 }
 
 // DecideAction only pedal
@@ -22,7 +26,11 @@ func (agent *BaselineAgent) DecideForces() {
 
 // DecideJoining accept all
 func (agent *BaselineAgent) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UUID]bool {
-	panic("to be implemented!")
+	decision := make(map[uuid.UUID]bool)
+	for _, agent := range pendingAgents {
+		decision[agent] = true
+	}
+	return decision
 }
 
 func (agent *BaselineAgent) ProposeDirection() utils.Coordinates {
@@ -42,16 +50,37 @@ func (agent *BaselineAgent) ProposeDirection() utils.Coordinates {
 //}
 
 // decideTargetLootBox find closest lootBox
-func (agent *BaselineAgent) decideTargetLootBox(lootBoxes map[uuid.UUID]objects.ILootBox) (objects.LootBox, error) {
-	panic("to be implemented!")
+func (agent *BaselineAgent) decideTargetLootBox(lootBoxes map[uuid.UUID]objects.ILootBox) (objects.ILootBox, error) {
+	var targetLootBox objects.ILootBox
+	minDistance := math.MaxFloat64
+	for _, lootBox := range lootBoxes {
+		dist := physics.ComputeDistance(agent.currentBike.GetPosition(), lootBox.GetPosition())
+		if dist < minDistance {
+			minDistance = dist
+			targetLootBox = lootBox
+		}
+	}
+	return targetLootBox, nil
 }
 
 // rankTargetProposals rank by distance
 func (agent *BaselineAgent) rankTargetProposals(proposedLootBox []objects.LootBox) (map[utils.Coordinates]float64, error) {
-	panic("to be implemented!")
+	// sort lootBox by distance
+	sort.Slice(proposedLootBox, func(i, j int) bool {
+		return physics.ComputeDistance(agent.currentBike.GetPosition(), proposedLootBox[i].GetPosition()) < physics.ComputeDistance(agent.currentBike.GetPosition(), proposedLootBox[j].GetPosition())
+	})
+	rank := make(map[utils.Coordinates]float64)
+	for i, lootBox := range proposedLootBox {
+		rank[lootBox.GetPosition()] = float64(i)
+	}
+	return rank, nil
 }
 
 // rankAgentReputation randomly rank agents
-func (agent *BaselineAgent) rankAgentsReputation([]objects.BaseBiker) (map[uuid.UUID]float64, error) {
-	panic("to be implemented!")
+func (agent *BaselineAgent) rankAgentsReputation(agentsOnBike []objects.BaseBiker) (map[uuid.UUID]float64, error) {
+	rank := make(map[uuid.UUID]float64)
+	for i, agent := range agentsOnBike {
+		rank[agent.GetID()] = float64(i)
+	}
+	return rank, nil
 }
