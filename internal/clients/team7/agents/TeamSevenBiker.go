669dfcf44651293288344bd87d853a87f8b7ea62
Basheq Tarifi <btarifi08@gmail.com>
2023-11-19 03:09:55 +0000

    Add voting framework and instantiation of BaseTeamSevenBiker

--- internal/clients/team7/agents/TeamSevenBiker.go
+++ internal/clients/team7/agents/TeamSevenBiker.go
@@ -1,7 +1,11 @@
 package agents
 
 import (
+	"SOMAS2023/internal/clients/team7/frameworks"
 	objects "SOMAS2023/internal/common/objects"
+	"SOMAS2023/internal/common/utils"
+
+	"github.com/google/uuid"
 )
 
 type ITeamSevenBiker interface {
@@ -9,5 +13,33 @@ type ITeamSevenBiker interface {
 }
 
 type BaseTeamSevenBiker struct {
-	*objects.BaseBiker
+	*objects.BaseBiker    // BaseBiker inherits functions from BaseAgent such as GetID(), GetAllMessages() and UpdateAgentInternalState()
+	navigationFramework   *frameworks.NavigationDecisionFramework
+	bikeDecisionFramework *frameworks.BikeDecisionFramework
+	opinionFramework      *frameworks.OpinionFramework
+	socialNetwork         *frameworks.SocialNetwork
+	votingFramework       *frameworks.VotingFramework
+}
+
+// Implement constructor for BaseTeamSevenBiker
+func NewBaseTeamSevenBiker(agentId uuid.UUID) *BaseTeamSevenBiker {
+	return &BaseTeamSevenBiker{
+		BaseBiker:             objects.GetBaseBiker(utils.GenerateRandomColour(), agentId),
+		navigationFramework:   frameworks.NewNavigationDecisionFramework(),
+		bikeDecisionFramework: frameworks.NewBikeDecisionFramework(),
+		opinionFramework:      frameworks.NewOpinionFramework(frameworks.OpinionFrameworkInputs{}),
+		socialNetwork:         frameworks.NewSocialNetwork(),
+		votingFramework:       frameworks.NewVotingFramework(),
+	}
+}
+
+// Override base biker functions
+func (biker *BaseTeamSevenBiker) DecideForce() {
+	navInputs := frameworks.NavigationInputs{
+		DesiredLootbox:  biker.NearestLoot(),
+		CurrentLocation: biker.GetLocation(),
+	}
+	navOutput := biker.navigationFramework.GetDecision(navInputs)
+
+	biker.SetForces(navOutput)
 }
