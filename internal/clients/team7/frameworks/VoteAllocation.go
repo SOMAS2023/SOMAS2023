aa63f9629433407b13c3bc3c78bdbce7286bb03e
daracos <daracostelloe@hotmail.com>
2023-11-24 14:36:32 +0000

    Added basic VoteAllocationWrapper

---
+++ internal/clients/team7/frameworks/VoteAllocation.go
@@ -0,0 +1,18 @@
+package frameworks
+
+import (
+	"github.com/google/uuid"
+)
+
+// func VoteOnAllocationWrapper(fellowBikers []objects.IBaseBiker) Vote {
+func VoteOnAllocationWrapper(voteInputs VoteInputs) Vote {
+	var vote map[uuid.UUID]interface{} // TODO: Import voting when rebase done and use IdVoteMap type.
+	var num_agents float64
+	num_agents = float64(len(voteInputs.Candidates))
+
+	for _, agent_id := range voteInputs.Candidates {
+		vote[agent_id] = 1 / num_agents
+	}
+
+	return Vote{result: vote}
+}
