e27413f43e9266728aee4f36bb720d33085b838c
ZihaoX3 <zx1420@ic.ac.uk>
2023-11-24 18:08:42 +0000

    Update BaselineAgent.go

--- internal/clients/team 8/BaselineAgent.go
+++ internal/clients/team 8/BaselineAgent.go
@@ -3,39 +3,18 @@ package team_8
 import (
 	"SOMAS2023/internal/common/objects"
 	"SOMAS2023/internal/common/utils"
+	"SOMAS2023/internal/common/voting"
 	"math/rand"
 
 	"github.com/google/uuid"
 )
 
-type BaselineAgent struct {
+type Agent8 struct {
 	objects.BaseBiker
 	currentBike *objects.MegaBike
 }
 
-// DecideAction only pedal
-func (agent *BaselineAgent) DecideAction() objects.BikerAction {
-	if not objects.leave {
-		return objects.Pedal
-	} else {
-		return objects.ChangeBike
-	}
-
-}
-
-func (agent *BaselineAgent) DecideForces() {
-	energyLevel := agent.GetEnergyLevel()
-
-	randomPedalForce := rand.Float64() * energyLevel
-	forces := utils.Forces{
-		Pedal:   randomPedalForce,
-		Brake:   0.0,
-		Turning: 0.0,
-	}
-	println("forces for each round", forces)
-}
-
-func (agent *BaselineAgent) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UUID]bool {
+func (agent *Agent8) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UUID]bool {
 	decision := make(map[uuid.UUID]bool)
 	trust := make(map[uuid.UUID]int)
 	for _, agent := range pendingAgents {
@@ -47,4 +26,36 @@ func (agent *BaselineAgent) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UU
 	return decision
 }
 
+func (agent *Agent8) ChangeBike() {
+	return
+}
 
+func (agent *Agent8) DecideAction() {
+
+	return objects.ChangeBike
+} //BikerAction
+
+func (agent *Agent8) ProposeDirection() {
+	//
+	return
+}
+
+func (agent *Agent8) FinalDirectionVote(proposals []uuid.UUID) voting.LootboxVoteMap {
+	return LootboxVoteMap
+}
+
+func (agent *Agent8) DecideForces() {
+	energyLevel := agent.GetEnergyLevel()
+
+	randomPedalForce := rand.Float64() * energyLevel
+	forces := utils.Forces{
+		Pedal:   randomPedalForce,
+		Brake:   0.0,
+		Turning: 0.0,
+	}
+	println("forces for each round", forces)
+}
+
+func (agent *Agent8) DecideAllocation() voting.IdVoteMap {
+
+}
