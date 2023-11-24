7b684b95fa2ecb9e9ae499e0c577b473349d920b
AnaBakoc <ana.bakoc.bgd@gmail.com>
2023-11-24 14:20:03 +0100

    VotingFramework addition

---
+++ internal/clients/team7/frameworks/VoteToAccept.go
@@ -0,0 +1,10 @@
+package frameworks
+
+func VoteToAcceptWrapper(voteInputs VoteInputs) Vote {
+	var vote Map
+	vote_decision := true
+	vote["decision"] = vote_decision
+	return Vote{result: vote}
+}
+
+// TODO: Add functions for voting to kick an agent off the bike.
