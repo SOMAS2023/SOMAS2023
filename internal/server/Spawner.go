11159430108eb8170c2e56e12bcdc96a5bf285df
samin50 <shaheen.amin.uk@gmail.com>
2023-11-25 15:23:24 +0000

    Visualiser ready to be merged

--- internal/server/Spawner.go
+++ internal/server/Spawner.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	agent5 "SOMAS2023/internal/clients/team_5"
 	"SOMAS2023/internal/common/objects"
 	"SOMAS2023/internal/common/utils"
 
@@ -18,8 +17,7 @@ func GetAgentGenerators() []baseserver.AgentGeneratorCountPair[objects.IBaseBike
 }
 
 func BikerAgentGenerator() objects.IBaseBiker {
-	return agent5.NewTeam5Agent(utils.GenerateRandomColour(), uuid.New())
-	// return objects.GetIBaseBiker(utils.GenerateRandomColour(), uuid.New())
+	return objects.GetIBaseBiker(utils.GenerateRandomColour(), uuid.New())
 }
 
 func (s *Server) spawnLootBox() {
