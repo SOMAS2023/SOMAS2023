3085b32d828ee50bb0e93ae38a02abf14bbc09c8
KerryZsz <106087842+KerryZsz@users.noreply.github.com>
2023-11-25 15:30:27 +0000

    Update agentGroup8.go

--- internal/clients/team8/agentGroup8.go
+++ internal/clients/team8/agentGroup8.go
@@ -2,6 +2,7 @@ package team_8
 
 import (
 	"SOMAS2023/internal/common/objects"
+	utils "SOMAS2023/internal/common/utils"
 	"SOMAS2023/internal/common/voting"
 
 	"github.com/google/uuid"
@@ -9,37 +10,127 @@ import (
 
 type Agent8 struct {
 	*objects.BaseBiker
+	ID           uuid.UUID
+	soughtColour utils.Colour
+	energyLevel  float64
+	gameState    IGameState
 }
 
-// in the MVP the biker's action defaults to pedaling (as it won't be able to change bikes)
-// in future implementations this function will be overridden by the agent's specific strategy
-// which will be used to determine whether to pedalor try to change bike
-func (bb *Agent8) DecideAction() objects.BikerAction {
-	//TODO： need to be implemented
-	return 0
-}
+// type BaseBiker struct {
+// 	// Fields from your existing structure
+// 	ID               uuid.UUID
+// 	soughtColour     string // Assuming utils.Colour is a string type
+// 	onBike           bool
+// 	energyLevel      float64
+// 	points           int
+// 	megaBikeId       uuid.UUID
+// 	gameState        IGameState
+// 	allocationParams ResourceAllocationParams
+// }
 
-// determine the forces (pedalling, breaking and turning)
-// in the MVP the pedalling force will be 1, the breaking 0 and the tunring is determined by the
-// location of the nearest lootbox
-
-// the function is passed in the id of the voted lootbox, for now ignored
-func (bb *Agent8) DecideForce(direction uuid.UUID) {
-	//TODO： need to be implemented
-}
-
-// an agent will have to rank the agents that are trying to join and that they will try to
-func (bb *Agent8) DecideJoining(pendingAgents []uuid.UUID) map[uuid.UUID]bool {
-	//TODO： need to be implemented
-	decision := make(map[uuid.UUID]bool)
-	for _, agent := range pendingAgents {
-		decision[agent] = true
-	}
-	return decision
-}
+type Colour string
 
 // decide which bike to go to
 // for now it just returns a random uuid
+
+// CalculateGiniIndexFromAB calculates the Gini index using the given values of A and B.
+func CalculateGiniIndexFromAB(A, B float64) float64 {
+	// Ensure that the denominator is not zero to avoid division by zero
+	if A+B == 0 {
+		return 0.0 // or handle this case according to your requirements
+	}
+
+	// Calculate the Gini index
+	giniIndex := A / (A + B)
+
+	return giniIndex
+}
+
+func (bb *Agent8) GetEnergyLevel() float64 {
+	return bb.energyLevel
+}
+
+// CalculateAverageEnergy calculates the average energy level for agents on a specific bike.
+func (bb *Agent8) CalculateAverageEnergy(bikeID uuid.UUID) float64 {
+	// Step 1: Get fellowBikers from the specified bike
+	fellowBikers := bb.gameState.GetMegaBikes()[bikeID].GetAgents()
+
+	// Step 2: Ensure there is at least one agent
+	if len(fellowBikers) == 0 {
+		return 0.0 // or handle this case according to your requirements
+	}
+
+	// Step 3: Calculate the sum of energy levels
+	sum := 0.0
+	for _, agent := range fellowBikers {
+		sum += agent.energyLevel
+	}
+
+	// Step 4: Calculate the average
+	average := sum / float64(len(fellowBikers))
+
+	return average
+}
+
+func (bb *Agent8) GetColour() utils.Colour {
+	return bb.soughtColour
+}
+
+// CountAgentsWithSameColour counts the number of agents with the same colour as the reference agent on a specific bike.
+func (bb *Agent8) CountAgentsWithSameColour(bikeID uuid.UUID) int {
+	// Step 1: Get reference colour from the BaseBiker
+	referenceColour := bb.GetColour()
+
+	// Step 2: Get fellowBikers from the specified bike
+	fellowBikers := bb.gameState.GetMegaBikes()[bikeID].GetAgents()
+
+	// Step 3: Ensure there is at least one agent
+	if len(fellowBikers) == 0 {
+		return 0 // or handle this case according to your requirements
+	}
+
+	// Step 4: Count agents with the same colour as the reference agent
+	count := 0
+	for _, agent := range fellowBikers {
+		if agent.soughtColour == referenceColour {
+			count++
+		}
+	}
+
+	return count
+}
+
+func (bb *Agent8) CalculateBordaScore() uuid.UUID {
+	// Get all the bikes from the game state
+	megaBikes := bb.gameState.GetMegaBikes()
+
+	// Initialize a map to store Borda scores for each bike
+	bordaScores := make(map[uuid.UUID]float64)
+
+	// Iterate through each bike
+	for bikeID, megabike := range megaBikes {
+		// Calculate the Borda score for the current bike
+		bordaScore := bb.CalculateAverageEnergy(bikeID) +
+			float64(bb.CountAgentsWithSameColour(bikeID)) +
+			CalculateGiniIndexFromAB(float64(bb.CountAgentsWithSameColour(bikeID)), float64(len(megaBikes.GetAgents())))
+
+		// Store the Borda score in the map
+		bordaScores[bikeID] = bordaScore
+	}
+
+	// Find the bike with the highest Borda score
+	var highestBordaScore float64
+	var winningBikeID uuid.UUID
+	for bikeID, score := range bordaScores {
+		if score > highestBordaScore {
+			highestBordaScore = score
+			winningBikeID = bikeID
+		}
+	}
+
+	return winningBikeID
+}
+
 func (bb *Agent8) ChangeBike() uuid.UUID {
 	//TODO： need to be implemented
 	return uuid.New()
