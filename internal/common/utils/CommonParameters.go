c10ddaf39cb34407ff568175c6c90bbed8b5e6a0
IshaanReni <ishaanreni@gmail.com>
2023-11-26 15:03:16 +0000

    small fixes made so that collisions between no biker megabikes and lootboxes are bypassed, normalisation in the cumulativedist function now works and winningAllocations is no longer always empty

--- internal/common/utils/CommonParameters.go
+++ internal/common/utils/CommonParameters.go
@@ -3,8 +3,8 @@ package utils
 /*
 Environment Parameters
 */
-const GridHeight float64 = 500.0
-const GridWidth float64 = 500.0
+const GridHeight float64 = 75.0
+const GridWidth float64 = 75.0
 const CollisionThreshold float64 = 7.0
 const Epsilon float64 = 0.01 // tolerance for FP rounding and checking if == 1.0
 
@@ -20,7 +20,7 @@ const AudiMaxForce float64 = 1.0  // The audi's force is equivalent to that of o
 
 const DragCoefficient float64 = 1.0 // Drag coefficient can be optimised in experimentation
 
-const MovingDepletion float64 = 1.0 // proportionality of energy loss
+const MovingDepletion float64 = 0.1 // proportionality of energy loss
 
 const LimboEnergyPenalty float64 = -0.25 // amount of energy lost per round when off a bike
 
