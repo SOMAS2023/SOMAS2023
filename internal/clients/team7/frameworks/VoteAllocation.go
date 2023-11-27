353bfc5638200289046dddce62ae9a689a6ab07e
daracos <daracostelloe@hotmail.com>
2023-11-27 21:42:11 +0000

    Made VoteAllocation allocate all resource to our bike.

--- internal/clients/team7/frameworks/VoteAllocation.go
+++ internal/clients/team7/frameworks/VoteAllocation.go
@@ -1,15 +1,15 @@
 package frameworks
 
-//"github.com/google/uuid"
-
-// func VoteOnAllocationWrapper(fellowBikers []objects.IBaseBiker) Vote {
+// Greedy implementation. Vote for our agent to get all of resource.
 func VoteOnAllocationWrapper(voteInputs VoteInputs) Vote {
 	var vote map[interface{}]interface{} // TODO: Import voting when rebase done and use IdVoteMap type.
-	var num_agents float64
-	num_agents = float64(len(voteInputs.Candidates))
 
 	for _, agent_id := range voteInputs.Candidates {
-		vote[agent_id] = 1 / num_agents
+		if agent_id == voteInputs.TeamSevenBikerId {
+			vote[agent_id] = 1
+		} else {
+			vote[agent_id] = 0
+		}
 	}
 
 	return Vote{result: vote}
