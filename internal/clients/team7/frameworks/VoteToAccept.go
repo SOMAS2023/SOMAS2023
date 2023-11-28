24c652077b4db60eddadcc2c844866f2d9759e02
daracos <daracostelloe@hotmail.com>
2023-11-28 09:43:53 +0000

    Added basic implementation of VoteToAccept.

--- internal/clients/team7/frameworks/VoteToAccept.go
+++ internal/clients/team7/frameworks/VoteToAccept.go
@@ -1,11 +1,11 @@
 package frameworks
 
-/*func VoteToAcceptWrapper(voteInputs VoteInputs) Vote {
-	var vote Map
+// Basic implementation. Accept all new agents
+func VoteToAcceptWrapper(voteInputs VoteInputs) Vote {
+	var vote map[interface{}]interface{}
 	vote_decision := true
 	vote["decision"] = vote_decision
 	return Vote{result: vote}
 }
 
 // TODO: Add functions for voting to kick an agent off the bike.
-*/
