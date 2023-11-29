0950983ef400f43b137eda49bf40a385257b140d
ih8gin <erictxl@outlook.com>
2023-11-29 00:59:08 +0000

    implement Reputation and ResourceAllocation

--- internal/clients/team_3/SmartAgent.go
+++ internal/clients/team_3/SmartAgent.go
@@ -20,19 +20,20 @@ type SmartAgent struct {
 	currentBike   *objects.MegaBike
 	targetLootBox objects.ILootBox
 	reputationMap map[uuid.UUID]reputation
-}
 
-type reputation struct {
-	recentContribution  float64
-	historyContribution float64
-	decisionSimilarity  float64
-	isSameColor         float64
-	energyGain          float64
-	energyRemain        float64
+	lootBoxCnt      float64
+	energySpent     float64
+	lastEnergyLevel float64
 }
 
 // DecideAction only pedal
 func (agent *SmartAgent) DecideAction() objects.BikerAction {
+	if agent.GetEnergyLevel() < agent.lastEnergyLevel {
+		agent.energySpent += agent.lastEnergyLevel - agent.GetEnergyLevel()
+	}
+	agent.lastEnergyLevel = agent.GetEnergyLevel()
+
+	agent.updateRepMap()
 	return objects.Pedal
 }
 
@@ -40,11 +41,11 @@ func (agent *SmartAgent) DecideAction() objects.BikerAction {
 func (agent *SmartAgent) DecideForces(direction uuid.UUID) {
 	energyLevel := agent.GetEnergyLevel() // 当前能量
 
-	randomPedalForce := rand.Float64() * energyLevel // 使用 rand 包生成随机的 pedal 力量，可以根据需要调整范围
+	pedalForce := rand.Float64() * energyLevel // 使用 rand 包生成随机的 pedal 力量，可以根据需要调整范围
 
 	// 因为force是一个struct,包括pedal, brake,和turning，因此需要一起定义，不能够只有pedal
 	forces := utils.Forces{
-		Pedal: randomPedalForce,
+		Pedal: pedalForce,
 		Brake: 0.0, // 这里默认刹车为 0
 		Turning: utils.TurningDecision{
 			SteerBike:     true,
@@ -78,20 +79,15 @@ func (agent *SmartAgent) FinalDirectionVote(proposals []uuid.UUID) voting.Lootbo
 	for i, pp := range proposals {
 		boxProposed[i] = boxesInMap[pp]
 	}
-	rank, e := agent.rankTargetProposals(boxProposed)
-	if e != nil {
-		panic("unexpected error!")
-	}
+	rank, _ := agent.rankTargetProposals(boxProposed)
 	return rank
 }
 
 func (agent *SmartAgent) DecideAllocation() voting.IdVoteMap {
+	agent.lootBoxCnt += 1
 	currentBike := agent.GetGameState().GetMegaBikes()[agent.GetMegaBikeId()]
-	rank, e := agent.rankAgentsReputation(currentBike.GetAgents())
-	if e != nil {
-		panic("unexpected error!")
-	}
-	return rank
+	vote, _ := agent.scoreAgentsForAllocation(currentBike.GetAgents())
+	return vote
 }
 
 // decideTargetLootBox find closest lootBox
@@ -126,11 +122,53 @@ func (agent *SmartAgent) rankTargetProposals(proposedLootBox []objects.ILootBox)
 	return rank, nil
 }
 
-// rankAgentReputation randomly rank agents
-func (agent *SmartAgent) rankAgentsReputation(agentsOnBike []objects.IBaseBiker) (map[uuid.UUID]float64, error) {
-	rank := make(map[uuid.UUID]float64)
-	for i, agent := range agentsOnBike {
-		rank[agent.GetID()] = float64(i)
+// rankAgentReputation if self energy level is low (below average cost for a lootBox), we follow 'Smallest First', else 'Ration'
+func (agent *SmartAgent) scoreAgentsForAllocation(agentsOnBike []objects.IBaseBiker) (map[uuid.UUID]float64, error) {
+	scores := make(map[uuid.UUID]float64)
+	totalScore := 0.0
+	if agent.energySpent/agent.lootBoxCnt > agent.GetEnergyLevel() {
+		// go 'Smallest First' strategy, only take energyRemain into consideration
+		for _, others := range agentsOnBike {
+			id := others.GetID()
+			score := agent.reputationMap[id].energyRemain
+			scores[others.GetID()] = score
+			totalScore += score
+		}
+	} else {
+		// go 'Ration' strategy, considering all facts
+		for _, others := range agentsOnBike {
+			id := others.GetID()
+			rep := agent.reputationMap[id]
+			score := rep.isSameColor/ // Cognitive dimension: is same belief?
+				+rep.historyContribution + rep.lootBoxGet/ // Pareto principle: give more energy to those with more outcome
+				+rep.recentContribution/ // Forgiveness: forgive agents pedal harder recently
+				-rep.energyGain/ // Equality: Agents received more energy before should get less this time
+				+rep.energyRemain // Need: Agents with lower energyLevel require more, try to meet their need
+			scores[others.GetID()] = score
+			totalScore += score
+		}
 	}
-	return rank, nil
+
+	// normalize scores
+	for id, score := range scores {
+		scores[id] = score / totalScore
+	}
+
+	return scores, nil
+}
+
+func (agent *SmartAgent) updateRepMap() {
+	if agent.reputationMap == nil {
+		agent.reputationMap = make(map[uuid.UUID]reputation)
+	}
+	for _, bikes := range agent.GetGameState().GetMegaBikes() {
+		for _, otherAgent := range bikes.GetAgents() {
+			rep, exist := agent.reputationMap[otherAgent.GetID()]
+			if !exist {
+				rep = reputation{}
+			}
+			rep.updateScore(otherAgent, agent.GetColour())
+			agent.reputationMap[otherAgent.GetID()] = rep
+		}
+	}
 }
---
+++ internal/clients/team_3/reputation.go
@@ -0,0 +1,53 @@
+package team_3
+
+import (
+	"SOMAS2023/internal/common/objects"
+	"SOMAS2023/internal/common/utils"
+	"math"
+)
+
+type reputation struct {
+	// score, 0~1
+	recentContribution  float64
+	historyContribution float64
+	//decisionSimilarity  float64
+	isSameColor  float64
+	lootBoxGet   float64
+	energyGain   float64
+	energyRemain float64
+
+	// memory or counter
+	pedalCnt          float64
+	lastEnergyLevel   float64
+	energyReceivedCnt float64
+	lootBoxGetCnt     float64
+}
+
+func (rep *reputation) updateScore(biker objects.IBaseBiker, preferredColor utils.Colour) {
+	// update memory
+	pedal := biker.GetForces().Pedal - biker.GetForces().Brake
+	rep.pedalCnt += pedal
+	if biker.GetEnergyLevel() > rep.lastEnergyLevel {
+		gain := biker.GetEnergyLevel() - rep.lastEnergyLevel
+		rep.energyReceivedCnt += gain
+		rep.lootBoxGetCnt += 1
+	}
+	rep.lastEnergyLevel = biker.GetEnergyLevel()
+
+	// update score
+	rep.recentContribution = normalize(pedal)
+	rep.historyContribution = normalize(rep.historyContribution)
+	rep.energyRemain = normalize(rep.lastEnergyLevel)
+	rep.energyGain = normalize(rep.energyReceivedCnt)
+	rep.lootBoxGet = normalize(rep.lootBoxGetCnt)
+	if biker.GetColour() == preferredColor {
+		rep.isSameColor = 1
+	} else {
+		rep.isSameColor = 0
+	}
+}
+
+func normalize(input float64) (output float64) {
+	output = 2.0/(1.0+math.Exp(-input)) - 1
+	return
+}
