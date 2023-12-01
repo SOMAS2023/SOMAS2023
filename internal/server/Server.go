f1a0446423bf974416076ca13a992512359e0711
Neagu <an720@ic.ac.uk>
2023-12-01 12:05:24 +0000

    latest simulation parameters

--- internal/server/Server.go
+++ internal/server/Server.go
@@ -12,9 +12,8 @@ import (
 	"github.com/google/uuid"
 )
 
-const LootBoxCount = BikerAgentCount * 2
-const MegaBikeCount = BikerAgentCount / 2
-const BikerAgentCount = 6
+const LootBoxCount = BikerAgentCount
+const MegaBikeCount = BikerAgentCount / 5
 
 type IBaseBikerServer interface {
 	baseserver.IServer[objects.IBaseBiker]
