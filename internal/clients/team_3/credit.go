2a08db1ac1720f66e34ab03933853d163c8d018a
ruiqi shen <102202178+JasonShen-SH@users.noreply.github.com>
2023-12-04 12:36:39 +0000

    Add files via upload

---
+++ internal/clients/team_3/credit.go
@@ -0,0 +1,32 @@
+package team_3
+
+import (
+	"SOMAS2023/internal/common/objects"
+)
+
+type credit struct {
+	// score
+	credit                   float64
+	consecutiveNegativeCount int
+
+	// memory
+	_credit float64
+}
+
+func (cre *credit) updatecredit(biker SmartAgent, agentsOnBike []objects.IBaseBiker) {
+	// update memory
+	if *(biker.whether_need_leader(agentsOnBike)) == 0.0 {
+		cre._credit = -1.0
+	} else {
+		cre._credit = 1.0
+	}
+
+	// update score
+	cre.credit += cre._credit
+
+	if cre._credit == -1.0 {
+		cre.consecutiveNegativeCount++
+	} else {
+		cre.consecutiveNegativeCount = 0
+	}
+}
