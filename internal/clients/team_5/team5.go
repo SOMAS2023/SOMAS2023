68fcae60baf39639e739b8a5e7a4c9edbde6caa3
ZackReeves <zlr20@ic.ac.uk>
2023-12-04 20:59:15 +0000

    Added leader voting

--- internal/clients/team_5/team5.go
+++ internal/clients/team_5/team5.go
@@ -33,12 +33,12 @@ func (t5 *team5Agent) UpdateAgentInternalState() {
 	t5.updateReputationOfAllAgents()
 }
 
-//Functions can be called in any scenario
-
 // needs fixing always democracy
 func (t5 *team5Agent) DecideGovernance() utils.Governance {
 	return utils.Democracy
 }
+
+//Functions can be called in any scenario
 
 // needs fixing never gets off bike
 func (t5 *team5Agent) DecideAction() objects.BikerAction {
@@ -76,30 +76,32 @@ func (t5 *team5Agent) VoteForKickout() map[uuid.UUID]int {
 	return voteResults
 }
 
-// needs fixing currently votes for first agent
 func (t5 *team5Agent) VoteDictator() voting.IdVoteMap {
 	votes := make(voting.IdVoteMap)
 	fellowBikers := t5.GetFellowBikers()
-	for i, fellowBiker := range fellowBikers {
-		if i == 0 {
-			votes[fellowBiker.GetID()] = 1.0
-		} else {
-			votes[fellowBiker.GetID()] = 0.0
+	var value float64 = 0
+	for _, fellowBiker := range fellowBikers {
+		value = t5.QueryReputation(fellowBiker.GetID())
+		if fellowBiker.GetColour() == t5.GetColour() {
+			value += 1
 		}
+
+		votes[fellowBiker.GetID()] = value
 	}
 	return votes
 }
 
-// needs fixing currently votes for first agent
 func (t5 *team5Agent) VoteLeader() voting.IdVoteMap {
 	votes := make(voting.IdVoteMap)
 	fellowBikers := t5.GetFellowBikers()
-	for i, fellowBiker := range fellowBikers {
-		if i == 0 {
-			votes[fellowBiker.GetID()] = 1.0
-		} else {
-			votes[fellowBiker.GetID()] = 0.0
+	var value float64 = 0
+	for _, fellowBiker := range fellowBikers {
+		value = t5.QueryReputation(fellowBiker.GetID())
+		if fellowBiker.GetColour() == t5.GetColour() {
+			value += 1
 		}
+
+		votes[fellowBiker.GetID()] = value
 	}
 	return votes
 
