6c24dae3661ed338e302752e66cd68c5c07ae187
Shaanukstar123 <shaanukag0@gmail.com>
2023-12-04 20:20:50 +0000

    Updated main.go from upstream

--- main.go
+++ main.go
@@ -7,12 +7,8 @@ import (
 
 func main() {
 	fmt.Println("Hello Agents")
-<<<<<<< HEAD
-	server.Initialize(500).Start()
-=======
 	s := server.Initialize(100)
 	s.FoundingInstitutions()
 	s.UpdateGameStates()
 	s.Start()
->>>>>>> dee9ec203d4c60a8651299808ce6a6d04c17b859
 }
