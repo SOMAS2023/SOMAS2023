584ef033f4f2659cb127755a32a45551afef029e
samin50 <shaheen.amin.uk@gmail.com>
2023-12-05 16:47:28 +0000

    set proper speed limits

--- visualiser/util/Constants.py
+++ visualiser/util/Constants.py
@@ -11,6 +11,8 @@ EPSILON = 8
 ENERGYTHRESHOLD = 0.1
 THEMEJSON = "visualiser/theme.json"
 JSONPATH = "game_dump.json"
+MAXSPEED = 50
+ITERATIONLENGTH = 100
 TEXT = {
     "FONT" : "Arial",
     "FONT_SIZE": 1,
