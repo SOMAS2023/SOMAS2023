ac621f8d80ce1c96dacee0ec1f4aec1c6143bd33
sheriff4000 <sheriff4000@gmail.com>
2023-12-05 23:09:22 +0000

    fix testfoundinginstitutions

--- internal/server/tests/sim_loop_test.go
+++ internal/server/tests/sim_loop_test.go
@@ -137,7 +137,10 @@ func TestFoundingInstitutions(t *testing.T) {
 		s.AddAgent(mockBiker)
 		mockBikers[i] = mockBiker
 	}
-
+	gs := s.NewGameStateDump(0)
+	for _, agent := range s.GetAgentMap() {
+		agent.UpdateGameState(gs)
+	}
 	s.FoundingInstitutions()
 
 	/* 	for _, agent := range s.GetAgentMap() {
