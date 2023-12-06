2f08b81bb36eebc1bd025c6c0fe5d5cfe6da9548
Harry Phillips <harryjamesphillips@gmail.com>
2023-12-06 20:42:27 +0000

    bruh

--- internal/common/objects/BaseBiker.go
+++ internal/common/objects/BaseBiker.go
@@ -455,7 +455,7 @@ func (bb *BaseBiker) CreatekickoutMessage() KickoutAgentMessage {
 	return KickoutAgentMessage{
 		BaseMessage: messaging.CreateMessage[IBaseBiker](bb, bb.GetFellowBikers()),
 		AgentId:     uuid.Nil,
-		kickout:     false,
+		Kickout:     false,
 	}
 }
 
@@ -555,7 +555,7 @@ func (bb *BaseBiker) HandleKickoutMessage(msg KickoutAgentMessage) {
 
 	// sender := msg.BaseMessage.GetSender()
 	// agentId := msg.AgentId
-	// kickout := msg.kickout
+	// kickout := msg.Kickout
 }
 
 func (bb *BaseBiker) HandleReputationMessage(msg ReputationOfAgentMessage) {
