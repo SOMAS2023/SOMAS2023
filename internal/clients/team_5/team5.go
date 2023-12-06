d2988f1d1419aef8f95f71cd48e98a36877bd45d
samin50 <shaheen.amin.uk@gmail.com>
2023-12-06 14:39:52 +0000

    Add group ID

--- internal/clients/team_5/team5.go
+++ internal/clients/team_5/team5.go
@@ -33,6 +33,7 @@ const (
 // Creates an instance of Team 5 Biker
 func NewTeam5Agent(totColours utils.Colour, bikeId uuid.UUID) *team5Agent {
 	baseBiker := objects.GetBaseBiker(totColours, bikeId) // Use the constructor function
+	baseBiker.GroupID = 5
 	// print
 	fmt.Println("team5Agent: newTeam5Agent: baseBiker: ", baseBiker)
 	return &team5Agent{
