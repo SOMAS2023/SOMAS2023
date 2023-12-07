de00e9396b69fe1094b97766a39752b7e166d444
zniihgnexy <151135278+zniihgnexy@users.noreply.github.com>
2023-12-07 14:06:16 +0000

    reputation matrix changed. not using the get forces function

--- internal/clients/team_4/baseline.go
+++ internal/clients/team_4/baseline.go
@@ -7,7 +7,6 @@ import (
 	"SOMAS2023/internal/common/voting"
 	"fmt"
 	"math"
-	"math/rand"
 	"sort"
 
 	"github.com/google/uuid"
@@ -519,7 +518,70 @@ func (agent *BaselineAgent) ChangeBike() uuid.UUID {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Reputation and Honesty Matrix Teams Must Implement these or similar functions
+//changed version
+
+func (agent *BaselineAgent) CalculateReputation() {
+	////////////////////////////
+	//  As the program I used for debugging invoked "padal" and "break" with values of 0, I conducted tests using random numbers.
+	// In case of an updated main program, I will need to adjust the parameters and expressions of the reputation matrix.
+	// The current version lacks real data during the debugging process.
+	////////////////////////////
+	megaBikes := agent.GetGameState().GetMegaBikes()
+
+	for _, bike := range megaBikes {
+		// Get all agents on MegaBike
+		fellowBikers := bike.GetAgents()
+
+		// Iterate over each agent on MegaBike, generate reputation assessment
+		for _, otherAgent := range fellowBikers {
+			// Exclude self
+			selfTest := otherAgent.GetID() //nolint
+			if selfTest == agent.GetID() {
+				agent.reputation[otherAgent.GetID()] = 1.0
+			}
+
+			// Monitor otherAgent's location
+			// location := otherAgent.GetLocation()
+			// RAP := otherAgent.GetResourceAllocationParams()
+			// fmt.Println("Agent ID:", otherAgent.GetID(), "Location:", location, "ResourceAllocationParams:", RAP)
+
+			// Monitor otherAgent's forces
+			historyenergy := agent.energyHistory[otherAgent.GetID()]
+			lastEnergy := 1.0
+			if len(historyenergy) >= 2 {
+				lastEnergy = historyenergy[len(historyenergy)-2]
+				// rest of your code
+			} else {
+				lastEnergy = 0.0
+			}
+			energyLevel := otherAgent.GetEnergyLevel()
+			ReputationEnergy := float64((lastEnergy)) / energyLevel //CAUTION: REMOVE THE RANDOM VALUE
+			//print("我是大猴子")
+			//fmt.Println("Agent ID:", otherAgent.GetID(), "Reputation_Forces:", ReputationEnergy)
+
+			// Monitor otherAgent's bike status
+			bikeStatus := otherAgent.GetBikeStatus()
+			// Convert the boolean value to float64 and print the result
+			ReputationBikeShift := 0.2
+			if bikeStatus {
+				ReputationBikeShift = 1.0
+			}
+			//fmt.Println("Agent ID:", otherAgent.GetID(), "Reputation_Bike_Shift", float64(ReputationBikeShift))
+
+			// Calculate Overall_reputation
+			OverallReputation := ReputationEnergy * ReputationBikeShift
+			//fmt.Println("Agent ID:", otherAgent.GetID(), "Overall Reputation:", OverallReputation)
+
+			// Store Overall_reputation in the reputation map
+			agent.reputation[otherAgent.GetID()] = OverallReputation
+		}
+	}
+	for agentID, agentReputation := range agent.reputation {
+		print("Agent ID: ", agentID.String(), ", Reputation: ", agentReputation, "\n")
+	}
+}
+
+/* // Reputation and Honesty Matrix Teams Must Implement these or similar functions
 
 func (agent *BaselineAgent) CalculateReputation() {
 	////////////////////////////
@@ -572,7 +634,7 @@ func (agent *BaselineAgent) CalculateReputation() {
 	// for agentID, agentReputation := range agent.reputation {
 	// 	print("Agent ID: ", agentID.String(), ", Reputation: ", agentReputation, "\n")
 	// }
-}
+} */
 
 func (agent *BaselineAgent) CalculateHonestyMatrix() {
 	for _, bike := range agent.GetGameState().GetMegaBikes() {
