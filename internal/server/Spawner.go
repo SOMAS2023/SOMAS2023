19b3cc53d80ac8f548710db209d65455ba832ac9
samin50 <shaheen.amin.uk@gmail.com>
2023-12-10 14:19:47 +0000

    fix small visualiser bug, add team 5 to spawned agents

--- internal/server/Spawner.go
+++ internal/server/Spawner.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"SOMAS2023/internal/clients/team1"
 	"SOMAS2023/internal/clients/team2"
 	team5Agent "SOMAS2023/internal/clients/team5"
 	"SOMAS2023/internal/clients/team8"
@@ -14,11 +15,11 @@ import (
 type AgentInitFunction func(baseBiker *objects.BaseBiker) objects.IBaseBiker
 
 var AgentInitFunctions = []AgentInitFunction{
-	nil, // Base Biker
-	// team1.GetBiker1,     // Team 1
+	// nil,                 // Base Biker
+	team1.GetBiker1,     // Team 1
 	team2.GetBiker,      // Team 2
-	team8.GetIBaseBiker, // Team 8
 	team5Agent.GetBiker, // Team 5
+	team8.GetIBaseBiker, // Team 8
 }
 
 func GetAgentGenerators() []baseserver.AgentGeneratorCountPair[objects.IBaseBiker] {
