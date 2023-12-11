597bbda8698f57b265fdc0bb12ae0ef33697c1a4
IshaanReni <ishaanreni@gmail.com>
2023-12-11 14:27:50 +0000

    fix

--- internal/server/Server.go
+++ internal/server/Server.go
@@ -13,7 +13,7 @@ import (
 )
 
 const LootBoxCount = BikerAgentCount * 2.5 // 2.5 lootboxes available per Agent
-const MegaBikeCount = BikerAgentCount / 5  // Megabikes should have 8 riders
+const MegaBikeCount = BikerAgentCount / 8  // Megabikes should have 8 riders
 const BikerAgentCount = 56                 // 56 agents in total
 
 type IBaseBikerServer interface {
