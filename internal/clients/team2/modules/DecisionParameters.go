bbd4ec04a2907d6074887c475a631b1f8051d42c
zniihgnexy <151135278+zniihgnexy@users.noreply.github.com>
2023-12-11 17:07:39 +0000

    only team4

--- internal/clients/team2/modules/DecisionParameters.go
+++
@@ -1,8 +0,0 @@
-package modules
-
-const (
-	KickThreshold                   = 0.2 // Threshold for kicking out a biker
-	AcceptThreshold                 = 0.8 // Threshold for accepting a biker
-	LeaveBikeThreshold              = 0.4 // Threshold for leaving a bike
-	EnergyToOptimalLootboxThreshold = 0.4 // Threshold of energy to vote for our optimal lootbox
-)
--- internal/server/Spawner.go
+++ internal/server/Spawner.go
@@ -1,10 +1,7 @@
 package server
 
 import (
-	"SOMAS2023/internal/clients/team1"
-	"SOMAS2023/internal/clients/team2"
-	team5Agent "SOMAS2023/internal/clients/team5"
-	"SOMAS2023/internal/clients/team8"
+	"SOMAS2023/internal/clients/team4"
 	"SOMAS2023/internal/common/objects"
 	"SOMAS2023/internal/common/utils"
 
@@ -15,10 +12,11 @@ import (
 type AgentInitFunction func(baseBiker *objects.BaseBiker) objects.IBaseBiker
 
 var AgentInitFunctions = []AgentInitFunction{
-	team1.GetBiker1,     // Team 1
-	team2.GetBiker,      // Team 2
-	team5Agent.GetBiker, // Team 5
-	team8.GetIBaseBiker, // Team 8
+	// team1.GetBiker1,     // Team 1
+	// team2.GetBiker,      // Team 2
+	// team5Agent.GetBiker, // Team 5
+	// team8.GetIBaseBiker, // Team 8
+	team4.GetBiker4,
 }
 
 func GetAgentGenerators() []baseserver.AgentGeneratorCountPair[objects.IBaseBiker] {
