8f0c79b61d1707bc0eca8d5023ae159c57742f79
Che Ruan <cr623@ic.ac.uk>
2023-12-11 02:18:41 +0000

    Co-authored-by: Linlangxie <Linlangxie@users.noreply.github.com>

---
+++ internal/clients/team6/trust.go
@@ -0,0 +1,74 @@
+package team6
+
+import (
+	"github.com/google/uuid"
+)
+
+type Trust struct {
+	reputations     float64
+	goal            float64
+	relativeSuccess float64
+	trust           float64
+}
+
+//func (bb *Team6Biker) GetReputations(agentID uuid.UUID) Trust {
+//	return bb.Trust[agentID]
+//}
+
+func (bb *Team6Biker) UpdateAgentgoal(agentID uuid.UUID) {
+
+	newTrust := Trust{
+		reputations:     bb.Trust[agentID].reputations,
+		goal:            bb.Trust[agentID].goal + 1,
+		relativeSuccess: bb.Trust[agentID].relativeSuccess,
+		trust:           bb.Trust[agentID].trust,
+	}
+	bb.Trust[agentID] = newTrust
+}
+
+func (bb *Team6Biker) UpdateRelativeSuccess(agentID uuid.UUID) {
+
+	newTrust := Trust{
+		reputations:     bb.Trust[agentID].reputations,
+		goal:            bb.Trust[agentID].goal,
+		relativeSuccess: bb.Trust[agentID].relativeSuccess,
+		trust:           bb.Trust[agentID].trust,
+	}
+	bb.Trust[agentID] = newTrust
+}
+
+// Update a certain agent's Trust
+func (bb *Team6Biker) UpdateTrust(agent uuid.UUID) {
+	_, ok := bb.Trust[agent]
+	if !ok {
+		//if we have no data on an agent, initialise to neutral
+		newTrust := Trust{
+			reputations:     1,
+			goal:            0,
+			relativeSuccess: 0,
+			trust:           1,
+		}
+		bb.Trust[agent] = newTrust
+	}
+
+	newTrust := Trust{
+		reputations:     bb.Trust[agent].reputations,
+		goal:            bb.Trust[agent].goal,
+		relativeSuccess: bb.Trust[agent].relativeSuccess,
+		trust:           bb.Trust[agent].relativeSuccess * (bb.Trust[agent].goal + bb.Trust[agent].relativeSuccess),
+	}
+
+	bb.Trust[agent] = newTrust
+}
+
+// Initialise
+func (bb *Team6Biker) setTrust() {
+
+	//bb.Trust = make(map[uuid.UUID]Trust)
+	for _, agent := range bb.GetGameState().GetAgents() {
+		//if (bb.GetID()!=agent.GetID()){
+		agentId := agent.GetID()
+		bb.UpdateTrust(agentId)
+		//}
+	}
+}
--- internal/server/Spawner.go
+++ internal/server/Spawner.go
@@ -3,6 +3,7 @@ package server
 import (
 	"SOMAS2023/internal/clients/team1"
 	"SOMAS2023/internal/clients/team2"
+	"SOMAS2023/internal/clients/team6"
 	"SOMAS2023/internal/clients/team8"
 	"SOMAS2023/internal/common/objects"
 	"SOMAS2023/internal/common/utils"
@@ -18,6 +19,7 @@ var AgentInitFunctions = []AgentInitFunction{
 	team1.GetBiker1,     // Team 1
 	team2.GetBiker,      // Team 2
 	team8.GetIBaseBiker, // Team 8
+	team6.InitialiseBiker6,
 }
 
 func GetAgentGenerators() []baseserver.AgentGeneratorCountPair[objects.IBaseBiker] {
