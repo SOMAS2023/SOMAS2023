815a945248e91cef345e248549d41714dd5c884c
Laurie <laurie.johnston@me.com>
2023-12-12 10:59:22 +0000

    Updated as accidental comment out of messaging

--- internal/server/SimLoop.go
+++ internal/server/SimLoop.go
@@ -169,7 +169,7 @@ func (s *Server) Start() {
 		gameStates = append(gameStates, s.RunSimLoop(utils.RoundIterations))
 		fmt.Printf("\nMain game loop finished.\n\n")
 		fmt.Printf("Messaging session started...\n\n")
-		// s.RunMessagingSession()
+		s.RunMessagingSession()
 		fmt.Printf("\nMessaging session completed\n\n")
 		fmt.Printf("Game Loop %d completed.\n", i)
 	}
