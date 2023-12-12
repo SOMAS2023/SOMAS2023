e603f49458e8431bc72a44b519a6d50b29299e89
GerandAi <99738457+GerandAi@users.noreply.github.com>
2023-12-12 10:51:34 +0000

    Update OpinionFramework.go (#27)

--- internal/clients/team7/frameworks/OpinionFramework.go
+++ internal/clients/team7/frameworks/OpinionFramework.go
@@ -1,31 +1,61 @@
 package frameworks
 
-/*
-Base interface for all opinion frameworks. This can be used to define
-different behaviours of opinion formation giving the agent different behaviours.
-*/
-type IOpinionFramework[I, SN, O any] interface {
-	GetOpinion(inputs I, socialNetwork SN) O
-}
+import (
+	"math"
+
+	"github.com/google/uuid"
+)
 
 type OpinionFrameworkInputs struct {
-}
-
-type OpinionFrameworkOutputs struct {
+	AgentOpinion map[uuid.UUID]float64
+	Mindset      float64
 }
 
 type OpinionFramework struct {
-	IOpinionFramework[OpinionFrameworkInputs, SocialNetwork, OpinionFrameworkOutputs]
-	inputs *OpinionFrameworkInputs
+	Inputs *OpinionFrameworkInputs
 }
 
-// Constructor for OpinionFramework
 func NewOpinionFramework(of OpinionFrameworkInputs) *OpinionFramework {
-	return &OpinionFramework{inputs: &of}
+	return &OpinionFramework{Inputs: &of}
 }
 
-func (of *OpinionFramework) GetOpinion(inputs OpinionFrameworkInputs, socialNetwork SocialNetwork) OpinionFrameworkOutputs {
-	of.inputs = &inputs
-	// TODO: Formulate opinion
-	return OpinionFrameworkOutputs{}
+func (of *OpinionFramework) GetOpinion() float64 {
+
+	i := len(of.Inputs.AgentOpinion)
+	μ := of.Inputs.Mindset
+
+	O := make([]float64, i)
+	for idx := range O {
+		O[idx] = of.Inputs.AgentOpinion[uuid.UUID]
+	}
+
+	W := make([]float64, i)
+	for idx := range W {
+		W[idx] = 1
+	}
+
+	A := make([]float64, i)
+	for idx := range A {
+		A[idx] = 1.0 - math.Abs(O[idx]-μ)/math.Max(μ, 1.0-μ)
+	}
+
+	for idx := range W {
+		W[idx] = W[idx] + W[idx]*A[idx]
+	}
+
+	rowSum := 0.0
+	for _, val := range W {
+		rowSum += val
+	}
+
+	for idx := range W {
+		W[idx] /= rowSum
+	}
+
+	o := 0.0
+	for idx := range W {
+		o += W[idx] * O[idx]
+	}
+
+	return o
 }
